6b949a2da30fb030faa1881ac33aa57a2a78595d
trpascua <ted.pascua.11@gmail.com>
2024-01-23 01:30:34 -0800

    ProductModel, example of how to get product list and get product by id

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 console.log("Hello World!?");
 const express = require('express');
-const mongoose  = require('mongoose');
-const Product = require('./models/productModel')
+const mongoose = require('mongoose');
+const Product = require('./models/productModel');
 const app =express()
 
 app.use(express.json());
@@ -9,6 +9,25 @@ app.use(express.json());
 //ROUTES
 app.get('/', (req, res) => {
     res.send('Hello Node API')
+})
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const products = await Product.findById(id);
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 })
 
 app.get('/blog', (req, res) => {
