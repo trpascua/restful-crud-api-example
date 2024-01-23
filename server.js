06552306c9bedcdf179952ae7f870cc86b0714e0
trpascua <ted.pascua.11@gmail.com>
2024-01-22 23:04:27 -0800

    initial setup: express install with get request example

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("Hello World!?");
+
+const express = require('express');
+const app =express()
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('Hello Node API')
+})
+
+app.listen(3000, ()=>{
+    console.log('Node API is running of port 3000!');
+})
+
+
